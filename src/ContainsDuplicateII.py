e9afe5ee4d95522aace7e019aa292aedd236aff5
xingyezhi <571887437@qq.com>
2016-04-14 23:40:09 +0800

    4-14

--- src/ContainsDuplicateII.py
+++ src/ContainsDuplicateII.py
@@ -2,9 +2,9 @@ __author__ = 'xingyezhi'
 # encoding: utf-8
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        
+        map={}
+        for i in range(len(nums)):
+            if nums[i] in map and (i-map[nums[i]])<=k:
+                return True
+            map[nums[i]]=i
+        return False
